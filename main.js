4be9b457bc1c01a99cf763ad1e6ab31c57eb3465
patrickbinda <patrickbinda91@gmail.com>
2022-11-17 11:24:23 +0100

    modification

---
+++ main.js
@@ -0,0 +1,50 @@
+// ceci est tun commentaire
+/* commentaire sur plusieurs lignes*/
+/*var username = "Binda";
+console.log(username);
+username = "pat";
+console.log(username);*/
+/* les operations
+ var x = 10;
+ var y = 5;
+ console.log(x + y);
+ console.log(x * y);
+ console.log(x - y);
+ console.log(x / y);
+ */
+
+ /* incrementation / decrementation
+x = 5;
+
+x++;
+console.log(x);
+x--;
+console.log(x);
+*/
+
+/*concatenation
+var str = "Binda" + "Patrick";
+console.log(str);
+*/
+/*
+var str = "patrick";
+str += "binda";
+console.log(str);
+*/
+/* longueur d'une chaine de caractere
+var str = "patrick binda";
+console.log(str.length);
+*/
+
+ conditions
+userAge = 17;
+if (userAge < 18){
+    console.log("je ne suis pas majeur");
+}else if (userAge == 18){
+    connsole.log("tu viens d'atteindre la majorité");
+}else{
+    console.log("je suis majeur");
+}
+
+
+
